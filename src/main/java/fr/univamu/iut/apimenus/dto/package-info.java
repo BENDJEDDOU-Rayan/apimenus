357c9e7055f113630782059d05f811a9f91fe38a
BENDJEDDOU-Rayan-2225011a <rayan.bendjeddou@etu.univ-amu.fr>
2024-04-04 22:22:32 +0200

    Ajouts des package-info.java

---
+++ src/main/java/fr/univamu/iut/apimenus/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contenant tous les DTO utile pour l'api
+ */
+package fr.univamu.iut.apimenus.dto;
---
+++ src/main/java/fr/univamu/iut/apimenus/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package principal contenant les classes constituants l'api
+ */
+package fr.univamu.iut.apimenus;
